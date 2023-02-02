cdfff2de5673c976d7e22decc05157b66eef5bad
gtsop <gtsop@protonmail.com>
2023-02-02 14:47:01 +0200

    modify base example to receive an email and message

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class MessageIn(BaseModel):
+    email: EmailStr
+    message: str
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/message")
+def post_message(input: MessageIn) -> MessageIn:
+    return input
