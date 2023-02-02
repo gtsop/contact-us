f65968a7361ea31bc407b9207952f286d4c22441
gtsop <gtsop@protonmail.com>
2023-02-02 20:52:51 +0200

    update test

--- app/app.py
+++ app/app.py
@@ -1,2 +1,6 @@
 class App:
-    pass
+    def create_message(self, email, body):
+        return { "email": email, "body": body}
+        
+    def list_messages(self):
+        return []
--- app/app_test.py
+++ app/app_test.py
@@ -2,3 +2,15 @@ from .app import App
 
 def test_app_instantiates():
     assert App()
+
+def test_app_creates_messages():
+    app = App()
+    message = app.create_message(email="foo", body="bar")
+
+    assert message == { "email": "foo", "body": "bar" }
+
+def test_app_lists_messages():
+    app = App()
+    messages = app.list_messages()
+
+    assert messages == []
