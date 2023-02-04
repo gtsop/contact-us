1c95f944c0a43902c368ce64f254a8b83e3ec1fa
gtsop <gtsop@protonmail.com>
2023-02-04 10:20:45 +0200

    implement typer example

--- cli/main.py
+++ cli/main.py
@@ -1,7 +1,17 @@
 import typer
 
-def main(name: str):
+cli = typer.Typer()
+
+@cli.command()
+def hello(name: str):
     print(f"Hello {name}")
 
+@cli.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
+
 if __name__ == "__main__":
-    typer.run(main)
+    cli()
