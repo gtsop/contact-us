3d35c146152b4e309d17722922886de785effcbe
gtsop <gtsop@protonmail.com>
2023-02-04 13:18:44 +0200

    assert the cli creates and lists messages

--- app/cli/cli.py
+++ app/cli/cli.py
@@ -6,13 +6,20 @@ from app.transmitter import StdOutTransmitter
 cli = typer.Typer()
 app = App(StdOutTransmitter())
 
+def create_cli():
+    return typer.Typer()
+
 @cli.command()
 def list_messages():
-    print("0 messages\n")
+    messages = app.list_messages()
+    print(f"{len(messages)} messages\n")
+    for index, message in enumerate(messages):
+        print(f'{index + 1}. email="{message.email}" body="{message.body}"')
 
 @cli.command()
 def create_message(email: str, body: str):
-    print(f'Created message: email="{email}" body="{body}"')
+    message = app.create_message(email=email, body=body)
+    print(f'Created message: email="{message.email}" body="{message.body}"')
 
 if __name__ == "__main__":
     cli()
--- app/cli/cli_test.py
+++ app/cli/cli_test.py
@@ -2,14 +2,22 @@ from typer.testing import CliRunner
 
 from .cli import cli
 
-runner = CliRunner()
-
-def test_cli():
-    result = runner.invoke(cli, ["list-messages"])
-    assert result.exit_code == 0
-    assert "0 messages\n" in result.stdout
-
 def test_cli_create_message():
+    runner = CliRunner()
     result = runner.invoke(cli, ["create-message", "foo@bar.com", "hello world"])
     assert result.exit_code == 0
     assert 'Created message: email="foo@bar.com" body="hello world"' in result.stdout
+
+def test_cli_list_messages():
+    runner = CliRunner()
+    result = runner.invoke(cli, ["list-messages"])
+
+    assert result.exit_code == 0
+    assert "0 messages\n" in result.stdout
+
+    result = runner.invoke(cli, ["create-message", "foo@bar.com", "hello world"])
+    result = runner.invoke(cli, ["create-message", "bar@foo.com", "goodbye world"])
+    result = runner.invoke(cli, ["list-messages"])
+
+    assert result.exit_code == 0
+    assert '2 messages\n\n1. email="foo@bar.com" body="hello world"\n2. email="bar@foo.com" body="goodbye world"' in result.stdout
