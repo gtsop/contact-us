58c780846aa96d3f5eb89053aa48f1fdd21f4304
gtsop <gtsop@protonmail.com>
2023-02-06 23:12:10 +0200

    fix app.py

--- contact_us/app/app.py
+++ contact_us/app/app.py
@@ -1,10 +1,13 @@
+from typing import Type
 from contact_us.app.message import Message
 from contact_us.app.transmitter import Transmitter
 from contact_us.app.storage import Storage, InMemoryStorage
 
-class App:
 
-    def __init__(self, transmitter: Transmitter, storage: Storage = InMemoryStorage):
+class App:
+    def __init__(
+        self, transmitter: Type[Transmitter], storage: type[Storage] = InMemoryStorage
+    ):
         self.transmitter = transmitter()
         self.storage = storage()
 
@@ -19,6 +22,6 @@ class App:
 
     def list_messages(self):
         return self.storage.all()
-    
+
     def list_unsent_messages(self):
         return [m for m in self.storage.all() if not m.is_sent]
