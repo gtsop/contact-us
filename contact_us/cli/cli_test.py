9304bc54679a6b68a29628239585333f7bb208d3
gtsop <gtsop@protonmail.com>
2023-02-07 18:40:57 +0200

    format code

--- contact_us/cli/cli_test.py
+++ contact_us/cli/cli_test.py
@@ -2,12 +2,14 @@ import pytest
 from contact_us.testing import create_cli
 from typer.testing import CliRunner
 
+
 def test_cli_create_message():
     cli = create_cli()
     runner = CliRunner()
     result = runner.invoke(cli.typer, ["create-message", "foo@bar.com", "hello world"])
     assert result.exit_code == 0
     assert 'Created message: email="foo@bar.com" body="hello world"' in result.stdout
+
 
 def test_cli_list_messages():
     cli = create_cli()
@@ -18,11 +20,15 @@ def test_cli_list_messages():
     assert "0 messages\n" in result.stdout
 
     result = runner.invoke(cli.typer, ["create-message", "foo@bar.com", "hello world"])
-    result = runner.invoke(cli.typer, ["create-message", "bar@foo.com", "goodbye world"])
+    result = runner.invoke(
+        cli.typer, ["create-message", "bar@foo.com", "goodbye world"]
+    )
 
     result = runner.invoke(cli.typer, ["list-messages"])
 
     assert result.exit_code == 0
-    assert ('2 messages\n\n'
+    assert (
+        "2 messages\n\n"
         '1. email="foo@bar.com" body="hello world"\n'
-        '2. email="bar@foo.com" body="goodbye world"') in result.stdout
+        '2. email="bar@foo.com" body="goodbye world"'
+    ) in result.stdout
